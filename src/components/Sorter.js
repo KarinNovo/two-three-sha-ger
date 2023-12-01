26ad4ee48dc08c94671fe48da3241c1547f14c1d
KarinNovo <84449277+KarinNovo@users.noreply.github.com>
2023-12-01 21:26:43 +0200

    Update Sorter.js

--- src/components/Sorter.js
+++ src/components/Sorter.js
@@ -1,39 +1,45 @@
-import { Badge, Dropdown, Button } from "react-bootstrap";
-import { useState } from "react";
+import { Dropdown } from 'react-bootstrap';
+import { useState } from 'react';
 
-export default function Sorter({ sortSelected }) {
+
+
+export default function Sorter({ onCategorySelected, sortSelected }) {
+  const [selectedItem, setSelectedItem] = useState('Select Category');
+
+  const categories = [
+    "Hostages, Victims & Survivors",
+    "Popular",
+    "Social Activism and Support",
+    "Human Rights",
+    "Cultural and Religious Insights",
+    "Media and Public Perception",
+    "Terrorism and Security",
+    "International Conflict",
+    "Influencers"
+  ];
+
   const handleSelect = (e) => {
-    sortSelected(e);
+    setSelectedItem(e);
+    if (categories.includes(e)) {
+      onCategorySelected(e);
+    }
   };
-
-  const [value, setValue] = useState("Editor's choice");
 
   return (
     <div>
       <Dropdown onSelect={handleSelect}>
-        <Dropdown.Toggle
-          variant="success"
-          id="dropdown-basic"
-          style={{
-            background: "rgb(127, 179, 255)",
-            borderColor: "rgb(127, 179, 255)",
-            color: "rgb(39, 39, 42)",
-          }}
-        >
-          {value}
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          {selectedItem}
         </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Item eventKey="Hostages, Victims & Survivors">Hostages, Victims & Survivors</Dropdown.Item>
-        <Dropdown.Item eventKey="Popular">Popular</Dropdown.Item>
-        <Dropdown.Item eventKey="Social Activism and Support">Social Activism and Support</Dropdown.Item>
-        <Dropdown.Item eventKey="Human Rights and Advocacy">Human Rights and Advocacy</Dropdown.Item>
-        <Dropdown.Item eventKey="Cultural and Religious Insights">Cultural and Religious Insights</Dropdown.Item>
-        <Dropdown.Item eventKey="Media and Public Perception">Media and Public Perception</Dropdown.Item>
-        <Dropdown.Item eventKey="Terrorism and Security">Terrorism and Security</Dropdown.Item>
-        <Dropdown.Item eventKey="Political Movements">Political Movements</Dropdown.Item>
-        <Dropdown.Item eventKey="International Conflict">International Conflict</Dropdown.Item>
-      </Dropdown.Menu>
+        <Dropdown.Menu>
+          <Dropdown.Header>Categories</Dropdown.Header>
+          {categories.map((category, index) => (
+            <Dropdown.Item key={index} eventKey={category}>
+              {category}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
       </Dropdown>
     </div>
   );
