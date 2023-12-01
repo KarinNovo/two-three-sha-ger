6da76544fd779ac39e1c74de95a8391496d56ec6
KarinNovo <84449277+KarinNovo@users.noreply.github.com>
2023-12-01 22:27:16 +0200

    Update categoryHashtagsMap.js

--- src/components/categoryHashtagsMap.js
+++ src/components/categoryHashtagsMap.js
@@ -1,11 +1,13 @@
 export const categoryHashtagsMap = {
-    "Human Rights": ["WarCrimes", "CeasefireNOW", "HumanRights", "AntiZionism", "Antisemitism", "HumanRights", "InnocentWomen", "InnocentChildren", "WomanRights", "humanrights"],
-    "Terrorism and Security": ["HamasisISIS", "TheWestIsNext", "TerrorSplaining", "Shieldren", "FreeGazaFromHamas", "WarCrime", "Nazism", "violence"],
-    "Media and Public Perception": ["Propagaza", "MediaBias", "Hypocrisy", "HumanitarianCrisisLie"],
-    "Popular": ["Woke", "StandWithIsrael", "StandWithJews", "HamasIsCancer", "JewishHumor"],
+    "Influencers": ["ellakenan", "KerenAzaria", "YosephHaddad",  "NoaTishby",]
+    "Hostages, Victims & Survivors" : [ "BringThemBack", "BringThemBackHome"]
+    "Human Rights": ["WarCrimes", "HumanRights", "WorldChildrensDay", "InnocentWomen", "InnocentChildren", "WomanRights", "Shieldren"],
+    "Terrorism and Security": ["HamasisISIS", "TheWestIsNext", "TerrorSplaining", "FreeGazaFromHamas", "WarCrime", "Nazism", "violence", "CeaseFire"],
+    "Media and Public Perception": ["Propagaza", "MediaBias", "Hypocrisy", "HumanitarianCrisisLie", "IDF spokesperson", "CeasefireNOW", "TheNewYorkTimes"],
+    "Popular": ["Woke", "StandWithIsrael", "StandWithJews", "HamasIsCancer","JewishHumor"],
     "Social Activism and Support": ["ProPalestineRally", "RedCross", "StandWithUs", "Activism", "PeaceInTheMiddleEast", "StopItNow"],
     "Cultural and Religious Insights": ["Westurn", "Islam", "Religion", "Jews", "Holocaust", "GazaLight", "HAMAS"],
-    "International Conflict": ["MeJewMeToo", "TheWestlsNext", "StopAntisemitism", "TheNewYorkTimes", "NoaTishby", "TimeToStandUpForIsrael", "Educateyourself"],
-    "Influencers": ["ellakenan", "KerenAzaria", "IDF spokesperson", "Yana Yanuchka", "WorldChildrensDay", "BringThemBack", "BringThemBackHome", "Hamasisisis", "freegazafromhamas", "YosephHaddad", "CeaseFire"]
+    "International Conflict": ["AntiZionism", "Antisemitism","MeJewMeToo", "StopAntisemitism", "TimeToStandUpForIsrael", "Educateyourself"],
+    
   };
   export default categoryHashtagsMap;
