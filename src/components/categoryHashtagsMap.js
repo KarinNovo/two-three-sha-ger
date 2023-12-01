ce2724f6388d5aae49ee417805c12c2a7c47a07e
KarinNovo <84449277+KarinNovo@users.noreply.github.com>
2023-12-01 22:33:30 +0200

    Update categoryHashtagsMap.js

--- src/components/categoryHashtagsMap.js
+++ src/components/categoryHashtagsMap.js
@@ -1,13 +1,13 @@
 export const categoryHashtagsMap = {
-    "Influencers": ["ellakenan", "KerenAzaria", "YosephHaddad",  "NoaTishby",]
+    "Influencers": ["ellakenan","DR.EliDavid", "YosephHaddad",  "NoaTishby", "KerenAzaria",]
     "Hostages, Victims & Survivors" : [ "BringThemBack", "BringThemBackHome"]
-    "Human Rights": ["WarCrimes", "HumanRights", "WorldChildrensDay", "InnocentWomen", "InnocentChildren", "WomanRights", "Shieldren"],
+    "Human Rights": ["WarCrimes", "HumanRights", "WorldChildrensDay", "InnocentWomen", "InnocentChildren", "WomanRights", "Shieldren", "UNRA", "UN"],
     "Terrorism and Security": ["HamasisISIS", "TheWestIsNext", "TerrorSplaining", "FreeGazaFromHamas", "WarCrime", "Nazism", "violence", "CeaseFire"],
     "Media and Public Perception": ["Propagaza", "MediaBias", "Hypocrisy", "HumanitarianCrisisLie", "IDF spokesperson", "CeasefireNOW", "TheNewYorkTimes"],
     "Popular": ["Woke", "StandWithIsrael", "StandWithJews", "HamasIsCancer","JewishHumor"],
     "Social Activism and Support": ["ProPalestineRally", "RedCross", "StandWithUs", "Activism", "PeaceInTheMiddleEast", "StopItNow"],
     "Cultural and Religious Insights": ["Westurn", "Islam", "Religion", "Jews", "Holocaust", "GazaLight", "HAMAS"],
     "International Conflict": ["AntiZionism", "Antisemitism","MeJewMeToo", "StopAntisemitism", "TimeToStandUpForIsrael", "Educateyourself"],
-    
+     
   };
   export default categoryHashtagsMap;
