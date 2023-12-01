8737f44ed3de46865ca0a32e164d46ee948a9e9c
KarinNovo <84449277+KarinNovo@users.noreply.github.com>
2023-12-01 22:39:20 +0200

    Update model.js

--- src/model/model.js
+++ src/model/model.js
@@ -689,8 +689,14 @@ export const posts = [
   },
    {
     filename: "701.jpeg",
-    title: "“Hi @UNRWA👋Recognize this badge? 👇 It is your employee, who also happens to be a Hamas terrorist. Another employee of yours held an Israeli child hostage in his attic for 50 days, almost starving him to death. #23shager Credit:@DrEliDavid",
+    title: "Hi @UNRWA👋Recognize this badge? 👇 It is your employee, who also happens to be a Hamas terrorist. Another employee of yours held an Israeli child hostage in his attic for 50 days, almost starving him to death. #23shager Credit:@DrEliDavid",
     tags: ["StandWithIsrael", "DR.EliDavid", "UNRA", "UN"],
+    category: "Influencers"
+  },
+  {
+    filename: "702.jpeg",
+    title: "Ever wondered why you're standing in long airport security lines? #23shager Credit:@DrEliDavid",
+    tags: ["DR.EliDavid", "tError", "PeaceInTheMiddleEast"],
     category: "Influencers"
   },
 ];
