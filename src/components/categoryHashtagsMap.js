26ca6443dade5eb469ea211875720363c1556d05
KarinNovo <84449277+KarinNovo@users.noreply.github.com>
2023-12-02 15:19:32 +0200

    Update categoryHashtagsMap.js

--- src/components/categoryHashtagsMap.js
+++ src/components/categoryHashtagsMap.js
@@ -1,13 +1,13 @@
 export const categoryHashtagsMap = {
     "Influencers": ["ellakenan","DrEliDavid", "YosephHaddad",  "NoaTishby", "KerenAzaria"],
     "Hostages, Victims & Survivors" : [ "BringThemBack", "BringThemBackHome"],
-    "Human Rights": ["WarCrimes", "HumanRights", "WorldChildrensDay", "InnocentWomen", "InnocentChildren", "WomanRights", "Shieldren", "UNRA", "UN"],
-    "Terrorism and Security": ["HamasisISIS", "TheWestIsNext", "TerrorSplaining", "FreeGazaFromHamas", "WarCrime", "Nazism", "violence", "CeaseFire"],
-    "Media and Public Perception": ["Propagaza", "MediaBias", "Hypocrisy", "HumanitarianCrisisLie", "IDF spokesperson", "CeasefireNOW", "TheNewYorkTimes"],
-    "Popular": ["Woke", "StandWithIsrael", "StandWithJews", "HamasIsCancer","JewishHumor"],
-    "Social Activism and Support": ["ProPalestineRally", "RedCross", "StandWithUs", "Activism", "PeaceInTheMiddleEast", "StopItNow"],
-    "Cultural and Religious Insights": ["Westurn", "Islam", "Religion", "Jews", "Holocaust", "GazaLight", "HAMAS"],
-    "International Conflict": ["AntiZionism", "Antisemitism","MeJewMeToo", "StopAntisemitism", "TimeToStandUpForIsrael", "Educateyourself"],
+    "Human Rights": ["WarCrimes", "HumanRights", "WorldChildrensDay", "InnocentWomen", "InnocentChildren", "WomanRights", "Shieldren", "UNRA", "UN","KillingInnocent", "GayRights", "RapeInnocent", "ElderAbuse"  ],
+    "Terrorism and Security": ["HamasisISIS", "TheWestIsNext", "TerrorSplaining", "FreeGazaFromHamas", "WarCrime", "Nazism", "violence", "CeaseFire",  "Massacre","GretaThunberg",   ],
+    "Media and Public Perception": ["Propagaza", "MediaBias", "Hypocrisy", "HumanitarianCrisisLie", "IDF spokesperson", "CeasefireNOW", "TheNewYorkTimes","BBC", ],
+    "Popular": ["Woke", "StandWithIsrael", "StandWithJews", "HamasIsCancer","JewishHumor", , "WeLoveIDF",  "Israel"],
+    "Social Activism and Support": ["ProPalestineRally", "RedCross", "StandWithUs", "Activism", "PeaceInTheMiddleEast", "StopItNow",  "StandWithJews"],
+    "Cultural and Religious Insights": ["Westurn", "Islam", "Religion", "Jews", "Holocaust", "GazaLight", "HAMAS", "IsraeliPeople", "LiberalValues" ],
+    "International Conflict": ["AntiZionism", "Antisemitism","MeJewMeToo", "StopAntisemitism", "TimeToStandUpForIsrael", "Educateyourself", "WakeUp" ],
      
   };
   export default categoryHashtagsMap;
