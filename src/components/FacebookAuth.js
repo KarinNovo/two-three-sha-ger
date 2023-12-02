b7abc8503f30552a6bc466789bc1f47c8b3e5db8
KarinNovo <karin15082@gmail.com>
2023-12-02 12:18:54 +0200

    123

---
+++ src/components/FacebookAuth.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+
+function FacebookLoginComponent() {
+
+  useEffect(() => {
+    // Load the Facebook SDK script
+    loadFacebookSDK();
+  }, []);
+
+  const loadFacebookSDK = () => {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '1503635350425037',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v15.0'
+      });
+
+      // Check user login status
+      FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+      });
+    };
+
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  };
+
+  const statusChangeCallback = (response) => {
+    if (response.status === 'connected') {
+      console.log('Logged in', response);
+    } else {
+      console.log('Please log in to Facebook');
+    }
+  };
+
+  const shareContent = () => {
+    FB.ui({
+      method: 'share',
+      href: 'https://2-3-sha-ger.com/sharedcontent',
+    }, function(response){});
+  };
+
+  return (
+    <div>
+      <button onClick={shareContent}>Share on Facebook</button>
+    </div>
+  );
+}
+
+export default FacebookLoginComponent;
